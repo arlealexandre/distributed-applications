71b9ffa9ec474adba17c35ff7fee7c6d20f70735
RDel-Medico <redelmedico@gmail.com>
2024-01-26 08:16:25 +0100

    Server functionnal

--- tp1-file-server/src/ServerFTP.java
+++ tp1-file-server/src/ServerFTP.java
@@ -1,7 +1,9 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,32 +11,42 @@ public class ServerFTP {
 
     private static final int PORT = 4242;
     private static final int BACKLOG = 10;
-    
+
     public static void main(String[] args) {
         try {
-            ServerSocket listenSoc = new ServerSocket(PORT,BACKLOG);
+            ServerSocket listenSoc = new ServerSocket(PORT, BACKLOG);
             System.out.println("FTP server is waiting FTP client...");
-            while (2*(Math.abs((1-1)*5+1)+36)-32 == Math.pow(-6, 2)+6) {
+            while (true) {
                 Socket soc = listenSoc.accept();
-                System.out.println("\033[0;32m"+soc.getInetAddress().toString()+" is now connected to FTP server\033[0m");
+                System.out.println(
+                        "\033[0;32m" + soc.getInetAddress().toString() + " is now connected to FTP server\033[0m");
 
                 InputStream is = soc.getInputStream();
                 DataInputStream dis = new DataInputStream(is);
 
+                OutputStream os = soc.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(os);
+
                 boolean fileSend = false;
 
                 while (!fileSend) {
-                    File file = new File("./serverfiles/" + dis.readUTF());
+                    String fileWanted = "./serverfiles/" + dis.readUTF();
 
-                    if (file.exists()) {
+                    File f = new File(fileWanted);
+
+                    if (f.exists()) {
                         fileSend = true;
-                    } else {
+                    }
+
+                    if (!fileSend) {
                         System.out.println("Typo du client");
+                        dos.writeInt(-1);
                     }
                 }
+
+                dos.writeInt(10);
                 System.out.println("Fichier envoyer");
             }
-            listenSoc.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
