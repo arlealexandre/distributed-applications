b4492324ec4ffb9d6c064bdb611444ee5a6e005b
RDel-Medico <redelmedico@gmail.com>
2024-01-26 09:35:58 +0100

    Optimized version functional

---
+++ tp1-file-server/src/ClientFTPOptimized.java
@@ -0,0 +1,89 @@
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientFTPOptimized {
+
+    private static final int CHUNK_SIZE = 512;
+    private static final int SERVER_PORT = 4242;
+    private static final String SERVER_HOST = "localhost";
+
+    public static void main(String[] args) {
+
+        try {
+            Socket soc = new Socket(SERVER_HOST, SERVER_PORT); // Creation of client socket
+
+            System.out.println("------------------------------------------------------------------------------------");
+            System.out.println("\033[0;32mYou're now connected to "+SERVER_HOST+":"+SERVER_PORT+" FTP server\033[0m");
+
+            Scanner sc = new Scanner(System.in);;
+            String filename = "";
+
+            /* Writing the filename into the socket stream to server */
+            OutputStream os = soc.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+
+            InputStream is = soc.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+
+            boolean requestOk = false;
+            int nbByteFile = 0;
+
+            while(!requestOk) {
+                System.out.println("Enter a filename: ");
+                filename = sc.nextLine();
+                
+                /*Envoie du nom du fichier souhaiter au serveur*/
+                dos.writeUTF(filename);
+
+                /*Reception du nombre de byte du fichier souhaiter (-1 = fichier inexistant) */
+                nbByteFile = dis.readInt();
+                
+                if (nbByteFile != -1) {
+                    requestOk = true;
+                    sc.close();
+                } else {
+                    System.out.println("Message from server: the file named '"+filename+"' does not exists.");
+                    System.out.println("------------");
+                }
+
+                
+            }
+
+            if (requestOk) {
+                File tempFile = new File("./localfiles/"+filename);
+                byte[] fileBytes;
+                int nbChunk = (nbByteFile - nbByteFile % CHUNK_SIZE)/CHUNK_SIZE;
+                FileOutputStream fo = new FileOutputStream(tempFile);
+                
+                for (int i = 0; i < nbChunk; i++) {
+                    fileBytes = dis.readNBytes(CHUNK_SIZE);
+                    fo.write(fileBytes);
+                }
+
+                fileBytes = dis.readNBytes(nbByteFile % CHUNK_SIZE);
+                fo.write(fileBytes);
+                
+                
+                if (tempFile.exists()) {
+                    System.out.println(tempFile.getName()+" has been created");
+                } else {
+                    System.err.println("Error while creating " + tempFile.getName());
+                }
+
+                fo.close();
+                soc.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ tp1-file-server/src/ServerFTPOptimized.java
@@ -0,0 +1,73 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerFTPOptimized {
+
+    private static final int PORT = 4242;
+    private static final int BACKLOG = 10;
+    private static final int CHUNK_SIZE = 512;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket listenSoc = new ServerSocket(PORT, BACKLOG);
+            System.out.println("FTP server is waiting FTP client...");
+            while (true) {
+                Socket soc = listenSoc.accept();
+                System.out.println(
+                        "\033[0;32m" + soc.getInetAddress().toString() + " is now connected to FTP server\033[0m");
+
+                InputStream is = soc.getInputStream();
+                DataInputStream dis = new DataInputStream(is);
+
+                OutputStream os = soc.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(os);
+
+                boolean fileSend = false;
+                File f = null;
+                String fileWanted = "";
+
+                while (!fileSend) {
+                    fileWanted = "./serverfiles/" + dis.readUTF();
+
+                    f = new File(fileWanted);
+
+                    if (f.exists()) {
+                        fileSend = true;
+                    }
+
+                    if (!fileSend) {
+                        System.out.println("Typo du client");
+                        dos.writeInt(-1);
+                    }
+                }
+
+                FileInputStream fis = new FileInputStream(fileWanted);
+                byte[] content = fis.readAllBytes();
+
+                /*Ecriture de la longeur en octet du fichier */
+                dos.writeInt(content.length);
+
+                int nbChunk = (content.length - content.length % CHUNK_SIZE)/CHUNK_SIZE;
+
+                for (int i = 0; i < nbChunk; i++) {
+                    dos.write(content, CHUNK_SIZE*i, CHUNK_SIZE);
+                }
+
+                dos.write(content, CHUNK_SIZE * nbChunk, content.length % CHUNK_SIZE);
+
+                System.out.println("Fichier envoyer");
+                
+                fis.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
